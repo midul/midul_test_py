7775c026aae86bde6b3a8179f058c2d993dec754
midul <midul.facebook@gmail.com>
2018-07-15 05:08:11 -0400

    Cleanup code

--- pic.py
+++
@@ -1,5 +0,0 @@
-words = word_tokenize(data)
-                wordsFiltered = []
-                for w in words:
-                    if w not in stopWords:
-                        wordsFiltered.append(w)
